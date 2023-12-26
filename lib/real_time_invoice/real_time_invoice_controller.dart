da4a6c4bb7fcace3569198ca6d2c2f1b4fe7eaa9
Mike <mikelu@mirrormedia.mg>
2023-12-26 14:41:42 +0800

    [Feat]:圖片load路徑調整

--- lib/real_time_invoice/real_time_invoice_controller.dart
+++ lib/real_time_invoice/real_time_invoice_controller.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-
 import 'package:get/get.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:real_time_invoice_widget/data/provider/election_data_provider.dart';
 import 'package:real_time_invoice_widget/model/election_data.dart';
 
@@ -9,8 +7,8 @@ class RealTimeInvoiceController extends GetxController {
   final ElectionDataProvider electionDataProvider = Get.find();
   final Rxn<ElectionData> rxElectionData = Rxn();
   Timer? fetchDataTimer;
-  PackageInfo? info;
-  final RxBool isPackage = true.obs;
+
+
 
   @override
   void onInit() async {
@@ -19,10 +17,7 @@ class RealTimeInvoiceController extends GetxController {
     fetchDataTimer = Timer.periodic(const Duration(seconds: 30), (Timer timer) {
       fetchElectionData();
     });
-    info = await PackageInfo.fromPlatform();
-    if (info?.packageName == 'com.example.realTimeInvoiceWidget') {
-      isPackage.value = false;
-    }
+
   }
 
   void fetchElectionData() async {
